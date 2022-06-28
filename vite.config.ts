c40a78b12c080b09030145a7cb63fc974326270a
Obrad Lacman <lacmansoftware@gmail.com>
2022-06-28 21:23:30 +0900

    fix: update api endpoint on vite.config.ts and available to use api from backend difrectly

--- vite.config.ts
+++ vite.config.ts
@@ -120,7 +120,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       proxy: {
         // 选项写法
         '/api': {
-          target: 'http://127.0.0.1:8000',
+          target: 'http://35.194.244.237:9998',
           changeOrigin: true,
           rewrite: path => path.replace(/^\/api/, '')
         }
