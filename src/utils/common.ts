f455ee0d6c11ff03eed9a559cdea29dd98ca145c
Obrad Lacman <lacmansoftware@gmail.com>
2022-06-29 23:43:18 +0900

    feat: add utils/common.ts to define global functions

---
+++ src/utils/common.ts
@@ -0,0 +1,48 @@
+import dict from '@/config/dictionary.json'
+
+export const inDict = (id, data) => {
+  return dict[data].find((item) => item.id === id)?.value
+}
+
+export const getAgeByBirthday = function (birthday) {
+  if (!birthday) {
+    return ''
+  }
+  const yearBirthday = new Date(birthday).getFullYear()
+  const monthBirthday = new Date(birthday).getMonth()
+  const dayBirthday = new Date(birthday).getDate()
+
+  const yearNow = new Date().getFullYear()
+  const monthNow = new Date().getMonth()
+  const dayNow = new Date().getDate()
+
+  let age = yearNow - yearBirthday
+  let flag = false
+
+  if (monthNow <= monthBirthday) {
+    if (monthNow == monthBirthday) {
+      if (dayNow < dayBirthday) {
+        age--
+        flag = true
+      }
+    } else {
+      age--
+    }
+  }
+  if (age < 0) {
+    return 0 + '歲'
+  }
+  let months = Math.floor(monthNow - monthBirthday)
+  if (months < 0) {
+    months = 12 + months
+  }
+  if (age == 0) {
+    if (months == 0 && flag) {
+      months = 11
+    } else if (months == 0) {
+      months = 1
+    }
+    return months + '月'
+  }
+  return age + '歲'
+}
