ecb341caebafb49042d82fe7ea71a5b5d0997abc
Obrad Lacman <lacmansoftware@gmail.com>
2022-07-02 22:28:31 +0900

    feat: update form components

--- src/views/Member/Info/MemberInfoIndex.vue
+++ src/views/Member/Info/MemberInfoIndex.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { ContentWrap } from '@/components/ContentWrap'
 import { Search } from '@/components/Search'
+import { Dialog } from '@/components/Dialog'
 import { useI18n } from '@/hooks/web/useI18n'
 import { ElButton, ElTag, ElLink } from 'element-plus'
 import { Table } from '@/components/Table'
-import { getTableListApi, delTableListApi } from '@/api/member'
+import { getTableListApi, saveTableApi, delTableListApi } from '@/api/member'
 import { useTable } from '@/hooks/web/useTable'
 import { MemberInfoTableData } from '@/api/member/types'
-import { reactive, ref, onMounted } from 'vue'
+import { reactive, ref, unref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useEmitt } from '@/hooks/web/useEmitt'
 import { CrudSchema, useCrudSchemas } from '@/hooks/web/useCrudSchemas'
@@ -15,10 +16,17 @@ import { inDict, getAgeByBirthday } from '@/utils/common'
 import { callIcon, msgIcon, plusIcon, mergeIcon } from '@/utils/iconList'
 import { searchConfig, crudConfig } from './index'
 import { getApi } from '@/api/member/index'
+import Write from './components/Write.vue'
+import Detail from './components/Detail.vue'
+import dict from '@/config/dictionary.json'
+import { useDictStoreWithOut } from '@/store/modules/dict'
+import { getPinyinCode } from '@/utils/common'
 
 defineOptions({
   name: 'MemberInfoIndex'
 })
+
+const dictStore = useDictStoreWithOut()
 
 const store = {
   certificate: ref<ComponentOptions[]>([]),
@@ -28,17 +36,20 @@ const store = {
   disease: ref<ComponentOptions[]>([])
 }
 
-const setStore = (key: string, url: string, valueField: string, labelField: string) => {
-  getApi(url).then((res: any) => {
-    store[key].value = res?.data.map((item) => ({
-      label: item[labelField],
-      value: item[valueField]
-    }))
-  })
+const getInOptionFormat = async (url: string, valueField: string, labelField: string) => {
+  const res = await getApi(url)
+  return res?.data.map((item) => ({
+    label: item[labelField],
+    value: item[valueField]
+  }))
+}
+
+const setStore = async (key: string, url: string, valueField: string, labelField: string) => {
+  store[key].value = await getInOptionFormat(url, valueField, labelField)
 }
 
 onMounted(() => {
-  setStore('certificate', '/sys/dict/type/MEMBER_Certificate', 'code', 'value')
+  // setStore('certificate', '/sys/dict/type/MEMBER_Certificate', 'code', 'value')
   setStore('level', '/sys/member/level', 'id', 'levelName')
   setStore('cardStatus', '/sys/dict/type/MEMBER_CardStatus', 'code', 'value')
   setStore('hospital', '/sys/hospital', 'id', 'name')
@@ -60,42 +71,85 @@ const { getList, setSearchParams } = methods
 
 getList()
 
-useEmitt({
-  name: 'getList',
-  callback: (type: string) => {
-    if (type === 'add') {
-      tableObject.currentPage = 1
-    }
-    getList()
-  }
-})
-
 const { t } = useI18n()
+
+const setPinyinCode = async (item: any) => {
+  const write = unref(writeRef)
+  const formData = (await write?.getFormData()) as any
+  const py = getPinyinCode(formData.name)
+
+  // console.log(write)
+  // write don't have a setValue() function here.
+  // write?.setValue({
+  //   pinyinCode: py
+  // })
+}
 
 const crudSchemas = reactive<CrudSchema[]>([
   {
     label: '操作',
     field: 'action',
-    width: '120px'
+    width: '120px',
+    form: {
+      show: false
+    }
   },
   {
     label: '姓名',
     field: 'memberName',
-    placeholder: '請填寫'
+    form: {
+      show: false
+    }
   },
   {
     field: 'name',
     label: '姓名',
     form: {
-      component: 'Input',
       colProps: {
-        span: 24
+        span: 8
+      },
+      componentProps: {
+        onKeyup: setPinyinCode,
+        placeholder: '請填寫'
       }
     },
     table: {
       show: false
     },
     search: {
+      component: 'Input',
+      colProps: {
+        span: 6
+      },
+      show: true
+    }
+  },
+  {
+    field: 'pinyinCode',
+    label: '姓名拼音碼',
+    form: {
+      component: 'Input',
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    label: '性別',
+    field: 'gender',
+    width: '60px',
+    form: {
+      component: 'Select',
+      componentProps: {
+        options: dict.sex
+      },
+      colProps: {
+        span: 8
+      },
       show: true
     }
   },
@@ -106,39 +160,292 @@ const crudSchemas = reactive<CrudSchema[]>([
     placeholder: '請填寫',
     form: {
       component: 'Input',
+      colProps: {
+        span: 8
+      },
       show: true
     },
     search: {
+      component: 'Input',
+      colProps: {
+        span: 6
+      },
       show: true
     }
   },
   {
-    label: '性別',
-    field: 'gender',
-    width: '60px'
+    label: '生日',
+    field: 'birthday',
+    width: '100px',
+    form: {
+      label: '生日（陽曆）',
+      component: 'DatePicker',
+      componentProps: {
+        style: { width: '100%' },
+        placeholder: ''
+      },
+      colProps: {
+        span: 8
+      },
+      show: true
+    }
+  },
+  {
+    field: 'birthdayLunar',
+    label: '生辰（農曆）',
+    form: {
+      component: 'DatePicker',
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
   },
   {
     label: '年齡',
     field: 'age',
-    width: '60px'
-  },
-  {
-    label: '生日',
-    field: 'birthday',
-    width: '100px'
+    width: '60px',
+    form: {
+      show: false
+    }
   },
   {
     field: 'identityType',
     label: '證件類型',
-    table: {
-      show: false
+    form: {
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      api: async () => {
+        const res = await getInOptionFormat('/sys/dict/type/MEMBER_Certificate', 'code', 'value')
+        return res
+      },
+      colProps: { span: 8 },
+      show: true
     },
+    table: { show: false },
     search: {
       component: 'Select',
       componentProps: {
         options: store.certificate
       },
+      colProps: { span: 6 },
       show: true
+    }
+  },
+  {
+    label: '證件號碼',
+    field: 'identityCode',
+    width: '100px',
+    placeholder: '請填寫',
+    form: {
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    search: {
+      component: 'Input',
+      colProps: { span: 6 },
+      show: true
+    }
+  },
+  {
+    field: 'country',
+    label: '國家',
+    form: {
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'nativePlace',
+    label: '籍貫',
+    form: {
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'nationality',
+    label: '民族',
+    form: {
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      componentProps: {
+        style: { width: '100%' }
+      },
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'occupation',
+    label: '職業',
+    form: {
+      component: 'Input',
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'education',
+    label: '文化程度',
+    form: {
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'isMarried',
+    label: '婚姻狀況',
+    form: {
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'hasChild',
+    label: '生育情況',
+    form: {
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'height',
+    label: '身高（厘米）',
+    form: {
+      component: 'Input',
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'weight',
+    label: '體重（公斤）',
+    form: {
+      component: 'Input',
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'memberSource',
+    label: '客戶來源',
+    form: {
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'recommender',
+    label: '來源詳情',
+    form: {
+      component: 'Input',
+      colProps: {
+        span: 8
+      },
+      show: true
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'createHospitalId',
+    label: '初登門店',
+    form: {
+      component: 'Input',
+      componentProps: {
+        disabled: true
+      },
+      colProps: {
+        span: 8
+      }
+    },
+    table: {
+      show: false
     }
   },
   {
@@ -146,65 +453,118 @@ const crudSchemas = reactive<CrudSchema[]>([
     field: 'archivesNo',
     width: '100px',
     placeholder: '請填寫',
-    search: {
+    form: {
       component: 'Input',
+      componentProps: {
+        disabled: true
+      },
+      colProps: {
+        span: 8
+      }
+    },
+    search: {
+      colProps: { span: 6 },
       show: true
+    }
+  },
+  {
+    field: 'otherPhone',
+    label: '其他電話',
+    form: {
+      component: 'Input',
+      colProps: {
+        span: 8
+      }
+    },
+    table: {
+      show: false
+    }
+  },
+  {
+    field: 'profileLocation',
+    label: '門店',
+    form: {
+      label: '檔案所在門店',
+      component: 'Select',
+      componentProps: {
+        style: { width: '100%' }
+      },
+      componentProps: {
+        options: store.hospital
+      },
+      colProps: { span: 16 }
+    },
+    table: { show: false },
+    search: {
+      component: 'Select',
+      componentProps: {
+        options: store.hospital
+      },
+      colProps: { span: 6 },
+      show: true
+    }
+  },
+  {
+    field: 'contactPreference',
+    label: '預約聯繫方式',
+    form: {
+      component: 'Radio',
+      componentProps: {
+        options: dict.member.contactPreference
+      },
+      colProps: {
+        span: 8
+      }
+    },
+    table: {
+      show: false
     }
   },
   {
     label: '檔案存放地',
     field: 'profileLocationName',
     width: '125px',
-    placeholder: '請選擇'
-  },
-  {
-    field: 'profileLocation',
-    label: '門店',
-    table: {
+    placeholder: '請選擇',
+    form: {
       show: false
-    },
-    search: {
-      component: 'Select',
-      componentProps: {
-        options: store.hospital
-      },
-      show: true
     }
   },
   {
     label: '證件類型',
     field: 'identityTypeName',
     width: '60px',
-    placeholder: '請選擇'
-  },
-  {
-    label: '證件號碼',
-    field: 'identityCode',
-    width: '100px',
-    placeholder: '請填寫',
-    search: {
-      component: 'Input',
-      show: true
+    placeholder: '請選擇',
+    form: {
+      show: false
     }
   },
   {
     label: '會員級別',
     field: 'levelName',
     width: '100px',
-    placeholder: '請選擇'
+    placeholder: '請選擇',
+    form: {
+      show: false
+    }
   },
   {
-    label: '會員級別',
+    label: '會員等級',
     field: 'memberLevel',
-
+    form: {
+      colProps: {
+        span: 8
+      }
+    },
     table: {
       show: false
     },
     search: {
       component: 'Select',
       componentProps: {
-        options: store.level
+        options: store.level,
+        style: { width: '100%' }
       },
+      colProps: { span: 6 },
       show: true
     }
   },
@@ -213,7 +573,7 @@ const crudSchemas = reactive<CrudSchema[]>([
     field: 'cardStatus',
     width: '100px',
     placeholder: '請選擇',
-
+    form: { show: false },
     table: {
       show: false
     },
@@ -222,6 +582,7 @@ const crudSchemas = reactive<CrudSchema[]>([
       componentProps: {
         options: store.cardStatus
       },
+      colProps: { span: 6 },
       show: true
     }
   },
@@ -229,62 +590,177 @@ const crudSchemas = reactive<CrudSchema[]>([
     label: '會員卡號',
     field: 'cardNum',
     width: '100px',
-    placeholder: '請填寫會員卡號'
+    placeholder: '請填寫會員卡號',
+    form: { show: false },
+    table: { show: false }
   },
   {
     field: 'memberCardNum',
     label: '會員卡號',
-    table: {
-      show: false
-    },
+    form: { show: false },
+    table: { show: false },
     search: {
       component: 'Input',
       componentProps: {
         options: store.level
       },
+      colProps: { span: 6 },
       show: true
     }
   },
   {
     label: '卡內餘額',
     field: 'balance',
-    width: '100px'
+    width: '100px',
+    form: {
+      colProps: { span: 8 },
+      show: false
+    }
   },
   {
     label: '辦卡時間',
     field: 'cardCreateTime',
-    width: '100px'
+    width: '100px',
+    form: { show: false }
   },
   {
     label: '創建人',
     field: 'createUser',
-    width: '100px'
+    width: '100px',
+    form: { show: false }
   },
   {
     label: '創建門店',
     field: 'createHospital',
-    width: '100px'
+    width: '100px',
+    form: { show: false }
   },
   {
     field: 'firstDisease',
     label: '病種',
+    form: { show: false },
     table: {
       show: false
     },
     search: {
-      component: 'Input',
+      component: 'Select',
       componentProps: {
         options: store.disease
       },
+      colProps: { span: 6 },
       show: true
     }
+  },
+  {
+    field: 'email',
+    label: '郵箱',
+    form: {
+      component: 'Input',
+      colProps: { span: 8 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'weibo',
+    label: '微博',
+    form: {
+      component: 'Input',
+      colProps: { span: 8 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'wechat',
+    label: '微信',
+    form: {
+      component: 'Input',
+      colProps: { span: 8 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'urgentContact',
+    label: '緊急聯繫人',
+    form: {
+      component: 'Input',
+      colProps: { span: 8 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'urgentContactTel',
+    label: '緊急聯繫人電話',
+    form: {
+      component: 'Input',
+      colProps: { span: 8 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'urgentContactRel',
+    label: '緊急聯繫人關係',
+    form: {
+      component: 'Input',
+      colProps: { span: 8 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'certificatesUrlFace',
+    label: '上傳身份證正面照片',
+    form: {
+      component: 'Input',
+      colProps: { span: 24 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'certificatesUrlBack',
+    label: '上傳身份證反面照片',
+    form: {
+      component: 'Input',
+      colProps: { span: 24 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'appointRemark',
+    label: '預約備註',
+    form: {
+      component: 'Input',
+      componentProps: {
+        type: 'textarea',
+        rows: 2
+      },
+      colProps: { span: 24 }
+    },
+    table: { show: false }
+  },
+  {
+    field: 'comment',
+    label: '備註',
+    form: {
+      component: 'Input',
+      componentProps: {
+        type: 'textarea',
+        rows: 2
+      },
+      colProps: { span: 24 }
+    },
+    table: { show: false }
   }
 ])
 
 const { allSchemas } = useCrudSchemas(crudSchemas)
 
+const dialogVisible = ref(false)
+
+const dialogTitle = ref('')
+
 const AddAction = () => {
-  push('/member/info-add')
+  dialogTitle.value = '新增客人'
+  tableObject.currentRow = null
+  dialogVisible.value = true
 }
 
 const delLoading = ref(false)
@@ -302,9 +778,18 @@ const delData = async (row: MemberInfoTableData | null, multiple: boolean) => {
   })
 }
 
-const action = (row: MemberInfoTableData, type: string) => {
-  push(`/example/example-${type}?id=${row.id}`)
+const actionType = ref('')
+
+const action = (row: TableData, type: string) => {
+  dialogTitle.value = type === 'edit' ? 'exampleDemo.edit' : 'exampleDemo.detail'
+  actionType.value = type
+  tableObject.currentRow = row
+  dialogVisible.value = true
 }
+
+const writeRef = ref<ComponentRef<typeof Write>>()
+
+const loading = ref(false)
 
 const outCall = (mobile: string) => {
   console.log('out going call: ', searchSchema[8].componentProps.options)
@@ -313,12 +798,35 @@ const outCall = (mobile: string) => {
 const sendMsg = (mobile: string) => {
   console.log('out going call: ', mobile)
 }
+
+const save = async () => {
+  const write = unref(writeRef)
+  await write?.elFormRef?.validate(async (isValid) => {
+    if (isValid) {
+      loading.value = true
+      const data = (await write?.getFormData()) as any
+      console.log(data)
+      const res = await saveTableApi(data)
+        .catch(() => {})
+        .finally(() => {
+          loading.value = false
+        })
+      if (res) {
+        dialogVisible.value = false
+        tableObject.currentPage = 1
+        getList()
+      }
+    }
+  })
+}
 </script>
 
 <template>
   <ContentWrap>
     <Search
       :schema="allSchemas.searchSchema"
+      :is-col="true"
+      :inline="false"
       @search="setSearchParams"
       @reset="setSearchParams"
       expand
@@ -369,6 +877,38 @@ const sendMsg = (mobile: string) => {
       </template>
     </Table>
   </ContentWrap>
+
+  <Dialog v-model="dialogVisible" :title="dialogTitle" width="90%">
+    <Write
+      v-if="actionType !== 'detail'"
+      ref="writeRef"
+      :form-schema="allSchemas.formSchema"
+      :current-row="tableObject.currentRow"
+    />
+
+    <Detail
+      v-if="actionType === 'detail'"
+      :detail-schema="allSchemas.detailSchema"
+      :current-row="tableObject.currentRow"
+    />
+
+    <!-- <template #header="{ titleId, titleClass }">
+      <div class="flex justify-between">
+        <h4 :id="titleId" :class="titleClass">This is a custom header!</h4>
+      </div>
+    </template>
+ -->
+    <template #footer>
+      <ElButton v-if="actionType !== 'detail'" type="primary" :loading="loading" @click="save">
+        {{ t('exampleDemo.save') }}
+      </ElButton>
+      <ElButton @click="dialogVisible = false">{{ t('dialogDemo.close') }}</ElButton>
+    </template>
+  </Dialog>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.el-input__wrapper {
+  width: 100%;
+}
+</style>
--- src/views/Member/Info/components/Write.vue
+++ src/views/Member/Info/components/Write.vue
@@ -1,51 +1,50 @@
 <script setup lang="ts">
 import { Form } from '@/components/Form'
+import { ElInput, ElSelect, ElOption } from 'element-plus'
 import { useForm } from '@/hooks/web/useForm'
 import { PropType, reactive, watch } from 'vue'
-import { TableData } from '@/api/table/types'
-import { useI18n } from '@/hooks/web/useI18n'
+import { MemberInfoTableData } from '@/api/member/types'
 import { useValidator } from '@/hooks/web/useValidator'
-import { writeConfig } from '@/views/Member/Info/index.ts'
 
-const { required } = useValidator()
+const { required, isMobile } = useValidator()
 
 const props = defineProps({
   currentRow: {
-    type: Object as PropType<Nullable<TableData>>,
+    type: Object as PropType<Nullable<MemberInfoTableData>>,
     default: () => null
+  },
+  formSchema: {
+    type: Array as PropType<FormSchema[]>,
+    default: () => []
   }
 })
 
-const { t } = useI18n()
-
-const schema = reactive<FormSchema[]>(writeConfig)
-
 const rules = reactive({
-  title: [required()],
-  author: [required()],
-  importance: [required()],
-  pageviews: [required()],
-  display_time: [required()],
-  content: [required()]
+  memberName: [required()],
+  pinyinCode: [required()],
+  gender: [required()],
+  // mobile: [required(), isMobile()],
+  mobile: [required()],
+  memberLevel: [required()],
+  birthday: [required()]
+  // title: [required()],
+  // author: [required()],
+  // importance: [required()],
+  // pageviews: [required()],
+  // display_time: [required()],
+  // content: [required()]
 })
 
 const { register, methods, elFormRef } = useForm({
-  schema
+  schema: props.formSchema
 })
 
 watch(
   () => props.currentRow,
   (currentRow) => {
     if (!currentRow) return
-    const { setValues, setSchema } = methods
+    const { setValues } = methods
     setValues(currentRow)
-    setSchema([
-      {
-        field: 'content',
-        path: 'componentProps.defaultHtml',
-        value: currentRow.content
-      }
-    ])
   },
   {
     deep: true,
@@ -57,8 +56,57 @@ defineExpose({
   elFormRef,
   getFormData: methods.getFormData
 })
+
+const temp = [
+  {
+    value: 'weg',
+    label: 'weweg'
+  }
+]
+
+const setPinyinCode = (item: any) => {
+  console.log('clicked ', item)
+}
 </script>
 
 <template>
-  <Form :rules="rules" @register="register" />
+  <Form :rules="rules" @register="register">
+    <template #birthdayLunar="form">
+      <div class="w-[100%] flex">
+        <ElInput v-model="form['birthdayLunar']" :readonly="true" />
+        <ElSelect v-model="form['birthdayLunar2']">
+          <ElOption
+            v-for="item in temp"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </ElSelect>
+      </div>
+    </template>
+    <template #hasChild="form">
+      <div class="w-[100%] flex">
+        <ElSelect v-model="form['hasChild']" class="flex-auto w-[30%]">
+          <ElOption
+            v-for="item in temp"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </ElSelect>
+        <ElInput v-model="form['boyNum']" class="flex-1" disabled />
+        <span class="flex-none mx-1">男</span>
+        <ElInput v-model="form['girlNum']" class="flex-1" disabled />
+        <span class="flex-none mx-1">女</span>
+      </div>
+    </template>
+
+    <!-- <template #birthday="form">
+      <div class="w-[100%] flex">
+        <ElInput v-model="form['birthday']['form']" placeholder="abc" />
+      </div>
+    </template> -->
+  </Form>
 </template>
+
+<style lang="less" scoped></style>
