f205f0d64a4e6f0505d558174be8aec029f14fa4
Obrad Lacman <lacmansoftware@gmail.com>
2022-07-04 00:50:33 +0900

    chore: merge form layout completed

--- src/views/Member/Info/components/Merge.vue
+++ src/views/Member/Info/components/Merge.vue
@@ -6,38 +6,38 @@ import { useForm } from '@/hooks/web/useForm'
 import { reactive, unref, ref, onMounted } from 'vue'
 import { ElButton } from 'element-plus'
 import { useValidator } from '@/hooks/web/useValidator'
+import { getApi } from '@/api/common'
 
 const { required } = useValidator()
 
 const { t } = useI18n()
 
-const restaurants = ref<Recordable[]>([])
-const memberNameSearch = (queryString: string, cb: Fn) => {
-  const results = queryString
-    ? restaurants.value.filter(createFilter(queryString))
-    : restaurants.value
-  // call callback function to return suggestions
-  cb(results)
+const sMember = ref<string[]>(['', ''])
+
+const memberNameSearch = async (queryString: string, cb: Fn) => {
+  const res = await getApi(`/member/info/query?keyWords=${queryString}`)
+  const result = res?.data.map((item) => ({
+    id: item.memberId,
+    value: item.memberName,
+    label: item.memberMobile
+  }))
+  cb(result)
 }
 
-const loadAll = () => {
-  return [
-    { value: 'vue', link: 'https://github.com/vuejs/vue' },
-    { value: 'element', link: 'https://github.com/ElemeFE/element' },
-    { value: 'cooking', link: 'https://github.com/ElemeFE/cooking' },
-    { value: 'mint-ui', link: 'https://github.com/ElemeFE/mint-ui' },
-    { value: 'vuex', link: 'https://github.com/vuejs/vuex' },
-    { value: 'vue-router', link: 'https://github.com/vuejs/vue-router' },
-    { value: 'babel', link: 'https://github.com/babel/babel' }
-  ]
+const handleMainSelect = (item: Recordable) => {
+  sMember.value[0] = item.id
+  const { setValues } = methods
+  setValues({
+    mainMemberMobile: item.label // mobile
+  })
 }
 
-onMounted(() => {
-  restaurants.value = loadAll()
-})
-
-const handleSelect = (item: Recordable) => {
-  console.log(item)
+const handleLessSelect = (item: Recordable) => {
+  sMember.value[1] = item.id
+  const { setValues } = methods
+  setValues({
+    lessMemberMobile: item.label // mobile
+  })
 }
 
 const schema = reactive<FormSchema[]>([
@@ -46,222 +46,69 @@ const schema = reactive<FormSchema[]>([
     label: '主賬號客人姓名/電話',
     component: 'Autocomplete',
     componentProps: {
+      style: 'width: 100%',
       fetchSuggestions: memberNameSearch,
-      onSelect: handleSelect,
+      onSelect: handleMainSelect,
       slots: {
         default: true
       }
-    }
+    },
+    formItemProps: {
+      rules: [required()]
+    },
+    colProps: { span: 12 }
   },
   {
-    field: 'field2',
-    label: t('formDemo.select'),
-    component: 'Select',
+    field: 'mainMemberMobile',
+    label: '對應客人電話',
+    component: 'Input',
     componentProps: {
-      options: [
-        {
-          label: 'option1',
-          value: '1'
-        },
-        {
-          label: 'option2',
-          value: '2'
-        }
-      ]
-    }
+      readonly: true
+    },
+    colProps: { span: 12 }
   },
   {
-    field: 'field3',
-    label: t('formDemo.radio'),
-    component: 'Radio',
+    field: 'lessMemberName',
+    label: '被合賬號客人/電話',
+    component: 'Autocomplete',
     componentProps: {
-      options: [
-        {
-          label: 'option-1',
-          value: '1'
-        },
-        {
-          label: 'option-2',
-          value: '2'
-        }
-      ]
-    }
+      style: 'width: 100%',
+      fetchSuggestions: memberNameSearch,
+      onSelect: handleLessSelect,
+      slots: {
+        default: true
+      }
+    },
+    formItemProps: {
+      rules: [required()]
+    },
+    colProps: { span: 12 }
   },
   {
-    field: 'field4',
-    label: t('formDemo.checkbox'),
-    component: 'Checkbox',
-    value: [],
+    field: 'lessMemberMobile',
+    label: '對應客人電話',
+    component: 'Input',
     componentProps: {
-      options: [
-        {
-          label: 'option-1',
-          value: '1'
-        },
-        {
-          label: 'option-2',
-          value: '2'
-        },
-        {
-          label: 'option-3',
-          value: '3'
-        }
-      ]
-    }
-  },
-  {
-    field: 'field5',
-    component: 'DatePicker',
-    label: t('formDemo.datePicker'),
-    componentProps: {
-      type: 'date'
-    }
-  },
-  {
-    field: 'field6',
-    component: 'TimeSelect',
-    label: t('formDemo.timeSelect')
+      readonly: true
+    },
+    colProps: { span: 12 }
   }
 ])
 
 const { register, methods, elFormRef } = useForm({
   schema
 })
-
-const changeLabelWidth = (width: number | string) => {
-  const { setProps } = methods
-  setProps({
-    labelWidth: width
-  })
-}
-
-const changeSize = (size: string) => {
-  const { setProps } = methods
-  setProps({
-    size
-  })
-}
-
-const changeDisabled = (bool: boolean) => {
-  const { setProps } = methods
-  setProps({
-    disabled: bool
-  })
-}
-
-const changeSchema = (del: boolean) => {
-  const { delSchema, addSchema } = methods
-  if (del) {
-    delSchema('field2')
-  } else if (!del && schema[1].field !== 'field2') {
-    addSchema(
-      {
-        field: 'field2',
-        label: t('formDemo.select'),
-        component: 'Select',
-        componentProps: {
-          options: [
-            {
-              label: 'option1',
-              value: '1'
-            },
-            {
-              label: 'option2',
-              value: '2'
-            }
-          ]
-        }
-      },
-      1
-    )
-  }
-}
-
-const setValue = (reset: boolean) => {
-  const { setValues } = methods
-  if (reset) {
-    unref(elFormRef)?.resetFields()
-  } else {
-    setValues({
-      field1: 'field1',
-      field2: '2',
-      field3: '2',
-      field4: ['1', '3'],
-      field5: '2022-01-27',
-      field6: '17:00'
-    })
-  }
-}
-
-const index = ref(7)
-
-const setLabel = () => {
-  const { setSchema } = methods
-  setSchema([
-    {
-      field: 'field2',
-      path: 'label',
-      value: `${t('formDemo.select')} ${index.value}`
-    },
-    {
-      field: 'field2',
-      path: 'componentProps.options',
-      value: [
-        {
-          label: 'option-1',
-          value: '1'
-        },
-        {
-          label: 'option-2',
-          value: '2'
-        },
-        {
-          label: 'option-3',
-          value: '3'
-        }
-      ]
-    }
-  ])
-  index.value++
-}
-
-const addItem = () => {
-  const { addSchema } = methods
-  if (unref(index) % 2 === 0) {
-    addSchema({
-      field: `field${unref(index)}`,
-      label: `${t('formDemo.input')}${unref(index)}`,
-      component: 'Input'
-    })
-  } else {
-    addSchema(
-      {
-        field: `field${unref(index)}`,
-        label: `${t('formDemo.input')}${unref(index)}`,
-        component: 'Input'
-      },
-      unref(index)
-    )
-  }
-  index.value++
-}
-
-const formValidation = () => {
-  unref(elFormRef)!.validate((isValid) => {
-    console.log(isValid)
-  })
-}
-
-const verifyReset = () => {
-  unref(elFormRef)?.resetFields()
-}
 </script>
 
 <template>
   <Form :schema="schema" @register="register">
     <template #mainMemberName-default="{ item }">
       <div class="value">{{ item.value }}</div>
-      <span class="link">{{ item.link }}</span>
+      <span class="label">{{ item.label }}</span>
+    </template>
+    <template #lessMemberName-default="{ item }">
+      <div class="value">{{ item.value }}</div>
+      <span class="label">{{ item.label }}</span>
     </template>
   </Form>
 </template>
