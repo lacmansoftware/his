491103e5ae10ce1ca351d832cd6eb01b4568b209
Obrad Lacman <lacmansoftware@gmail.com>
2022-07-11 05:45:41 +0900

    feat: add recipel/online/input/index

---
+++ src/views/Recipel/Online/Input/Index.vue
@@ -0,0 +1,752 @@
+<script setup lang="ts">
+import { reactive, ref, unref, onMounted, watch, computed, nextTick, h } from 'vue'
+import { ElButton, ElLink, ElMessage, ElTag } from 'element-plus'
+import { useRouter } from 'vue-router'
+import { ContentWrap } from '@/components/ContentWrap'
+import { Search } from '@/components/Search'
+import { Dialog } from '@/components/Dialog'
+import { Table } from '@/components/Table'
+import { useI18n } from '@/hooks/web/useI18n'
+import { useEmitt } from '@/hooks/web/useEmitt'
+import { useTable } from '@/hooks/web/useTable'
+import { CrudSchema, useCrudSchemas } from '@/hooks/web/useCrudSchemas'
+import { useValidator } from '@/hooks/web/useValidator'
+import { inDict, getAgeByBirthday, getInOptionFormat } from '@/utils/common'
+import { plusIcon, deleteIcon } from '@/utils/iconList'
+import Write from './components/Write.vue'
+import dict from '@/config/dictionary.json'
+import { useDictStoreWithOut } from '@/store/modules/dict'
+
+import { getTableListApi, delTableListApi, saveTableApi } from '@/api/recipel/online/input'
+import { SMSTemplateData } from '@/api/recipel/online/input/types'
+import { getApi } from '@/api/common'
+
+defineOptions({
+  name: 'SMSSendIndex'
+})
+const { required, isMobile } = useValidator()
+const dictStore = useDictStoreWithOut()
+
+const store = {
+  type: ref<ComponentOptions[]>([])
+}
+
+const setStore = async (key: string, url: string, valueField: string, labelField: string) => {
+  store[key].value = await getInOptionFormat(url, valueField, labelField)
+}
+
+onMounted(async () => {
+  setStore('type', '/sys/dict/type/sms_tmp_type', 'code', 'value')
+})
+
+const { push } = useRouter()
+
+const { register, tableObject, methods } = useTable<MemberInfoTableData>({
+  getListApi: getTableListApi,
+  delListApi: delTableListApi,
+  response: {
+    list: 'data',
+    total: 'total'
+  }
+})
+
+const { getList, setSearchParams } = methods
+
+getList()
+
+const { t } = useI18n()
+
+const crudSchemas = reactive<CrudSchema[]>([
+  {
+    label: '操作',
+    field: 'action',
+    width: '120px',
+    form: { show: false }
+  },
+  {
+    label: '附加診費',
+    field: 'consFee',
+    width: '55px'
+  },
+  {
+    label: '進度狀態',
+    field: 'status',
+    width: '55px',
+    formatter: function (row) {
+      const photoStatus = row.photoStatus.toLowerCase()
+      if (photoStatus === 'reject') {
+        return '已駁回'
+      }
+      return inDict(row.photoStatus, 'visitStatus')
+    }
+  },
+  {
+    label: '客戶姓名',
+    field: 'memberName',
+    width: '55px'
+  },
+  {
+    label: '客人手機',
+    field: 'memberMobile',
+    width: '78px'
+  },
+  {
+    label: '微信昵稱',
+    field: 'nickName',
+    width: '60px'
+  },
+  {
+    label: '性別',
+    field: 'memberSex',
+    width: '40px',
+    formatter: function (row) {
+      return inDict(row.memberSex, 'sex')
+    }
+  },
+  {
+    label: '年齡',
+    field: 'memberAge',
+    width: '30px'
+  },
+  {
+    label: '大夫姓名',
+    field: 'doctorName',
+    width: '55px'
+  },
+  {
+    label: '大夫手機',
+    field: 'doctorMobile',
+    width: '78px'
+  },
+  {
+    label: '錄方狀態',
+    field: 'recipelStatus',
+    width: '55px',
+    formatter: function (row) {
+      return inDIct(row.recipelStatus, 'recipelInputStatus')
+    }
+  },
+  {
+    label: '照片數量',
+    field: 'files',
+    width: '40px',
+    formatter: function (row) {
+      if (row.files && row.files !== '--' && row.files !== 'null') {
+        return row.files.split(',').length
+      } else {
+        return '0'
+      }
+    }
+  },
+  {
+    label: '處方數量',
+    field: 'recipelAmount',
+    width: '40px'
+  },
+  {
+    label: '錄方方式',
+    field: 'type',
+    width: '60px',
+    formatter: function (row) {
+      return inDIct(row.type, 'onlineRecipelType')
+    }
+  },
+  {
+    label: '錄方人',
+    field: 'recipelUser',
+    width: '40px'
+  },
+  {
+    label: '審核時間',
+    field: 'auditTime',
+    width: '90px'
+  },
+  {
+    label: '提交時間',
+    field: 'pullTime',
+    width: '90px'
+  },
+  {
+    label: '創建時間',
+    field: 'createTime',
+    width: '90px'
+  },
+  {
+    label: '付款狀態',
+    field: 'status',
+    width: '55px',
+    formatter: function (row) {
+      if (row.status == 'A') {
+        return '已付款'
+      } else {
+        return '未付款'
+      }
+    }
+  },
+
+  // Search Schema
+
+  {
+    field: 'memberName',
+    label: '客人姓名',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'Input',
+      componentProps: {},
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'memberMobile',
+    label: '客人手機',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'Input',
+      componentProps: {},
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'doctorName',
+    label: '大夫姓名',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'Input',
+      componentProps: {},
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'recipelStatus',
+    label: '錄方狀態',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'Select',
+      componentProps: {
+        options: dict.recipelInputStatus
+      },
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'offlineType',
+    label: '門診類型',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'Select',
+      componentProps: {
+        options: dict.offlineRecipelType
+      },
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'registerTimeStart',
+    label: '挂號日期',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'DatePicker',
+      componentProps: {
+        type: 'date',
+        valueFormat: 'YYYY-MM-DD'
+      },
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'registerTimeEnd',
+    label: '到',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'DatePicker',
+      componentProps: {
+        type: 'date',
+        valueFormat: 'YYYY-MM-DD'
+      },
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'createTimeStart',
+    label: '創建日期',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'DatePicker',
+      componentProps: {
+        type: 'date',
+        valueFormat: 'YYYY-MM-DD'
+      },
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'createTimeEnd',
+    label: '到',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'DatePicker',
+      componentProps: {
+        type: 'date',
+        valueFormat: 'YYYY-MM-DD'
+      },
+      colProps: { span: 6 }
+    }
+  },
+  {
+    field: 'insurStatus',
+    label: '保險客人',
+    form: { show: false },
+    table: { show: false },
+    search: {
+      show: true,
+      component: 'Checkbox',
+      componentProps: {
+        options: dict.isInsur
+      },
+      colProps: { span: 6 },
+      value: []
+    }
+  }
+
+  // // Form schema
+  // // auto complete
+  // {
+  //   field: 'doctorSearch',
+  //   label: '醫生：',
+  //   form: {
+  //     component: 'Autocomplete',
+  //     componentProps: {
+  //       style: 'width: 100%',
+  //       triggerOnFocus: false,
+  //       fetchSuggestions: async (queryString: string, cb: Fn) => {
+  //         const res = await getApi(`/doctor/query?keyWords=${queryString}`)
+  //         const result = res?.data.map((item) => ({
+  //           ...item,
+  //           value: item.doctorName,
+  //           label: item.doctorMobile
+  //         }))
+  //         cb(result)
+  //       },
+  //       onSelect: (item: Recordable) => {},
+  //       slots: {
+  //         default: true
+  //       },
+  //       placeholder: '姓名/手機'
+  //     },
+  //     colProps: { span: 12 },
+  //     show: true
+  //   }
+  // },
+  // {
+  //   field: 'memberSearch',
+  //   label: '患者：',
+  //   form: {
+  //     component: 'Autocomplete',
+  //     componentProps: {
+  //       style: 'width: 100%',
+  //       triggerOnFocus: false,
+  //       fetchSuggestions: async (queryString: string, cb: Fn) => {
+  //         const res = await getApi(`/member/info/query?keyWords=${queryString}`)
+  //         const result = res?.data.map((item) => ({
+  //           ...item,
+  //           value: item.memberName
+  //         }))
+  //         cb(result)
+  //       },
+  //       onSelect: handleMemberSelected,
+  //       slots: {
+  //         default: true
+  //       },
+  //       placeholder: '姓名/手機'
+  //     },
+  //     colProps: { span: 12 },
+  //     show: true
+  //   }
+  // },
+
+  // // member info
+  // {
+  //   field: 'memberName',
+  //   label: '姓名：',
+  //   table: { show: false },
+  //   form: {
+  //     show: true,
+  //     component: 'Input',
+  //     componentProps: {
+  //       readonly: true
+  //     },
+  //     colProps: { span: 4 }
+  //   }
+  // },
+  // {
+  //   field: 'memberId',
+  //   label: '100px',
+  //   table: { show: false },
+  //   form: {
+  //     show: true,
+  //     component: 'Hidden',
+  //     componentProps: {
+  //       readonly: true
+  //     },
+  //     colProps: { span: 0 }
+  //   }
+  // },
+  // {
+  //   field: 'memberSex',
+  //   label: '性別：',
+  //   table: { show: false },
+  //   form: {
+  //     show: true,
+  //     component: 'Input',
+  //     componentProps: {
+  //       readonly: true
+  //     },
+  //     colProps: { span: 4 }
+  //   }
+  // },
+  // {
+  //   field: 'memberMobile',
+  //   label: '手機：',
+  //   table: { show: false },
+  //   form: {
+  //     show: true,
+  //     component: 'Input',
+  //     componentProps: {
+  //       readonly: true
+  //     },
+  //     colProps: { span: 4 }
+  //   }
+  // },
+  // {
+  //   field: 'memberBirthday',
+  //   label: '生日：',
+  //   table: { show: false },
+  //   form: {
+  //     show: true,
+  //     component: 'Input',
+  //     componentProps: {
+  //       readonly: true
+  //     },
+  //     colProps: { span: 4 }
+  //   }
+  // },
+  // {
+  //   field: 'memberAge',
+  //   label: '年齡：',
+  //   table: { show: false },
+  //   form: {
+  //     show: true,
+  //     component: 'Input',
+  //     componentProps: {
+  //       readonly: true
+  //     },
+  //     colProps: { span: 4 }
+  //   }
+  // },
+  // {
+  //   field: 'recordDivider',
+  //   label: '書寫病案',
+  //   table: { show: false },
+  //   form: {
+  //     show: true,
+  //     component: 'Divider'
+  //   }
+  // },
+  // {
+  //   field: 'slaveSymptom',
+  //   label: '診斷',
+  //   form: {
+  //     show: true,
+  //     component: 'Input',
+  //     componentProps: {
+  //       type: 'textarea',
+  //       rows: 2,
+  //       placeholder: '請輸入內容'
+  //     },
+  //     colProps: { span: 12 },
+  //     formItemProps: {
+  //       rules: [required()]
+  //     }
+  //   },
+  //   table: { show: false }
+  // },
+  // {
+  //   field: 'medicalHistory',
+  //   label: '主訴及現病史',
+  //   form: {
+  //     show: true,
+  //     component: 'Input',
+  //     componentProps: {
+  //       type: 'textarea',
+  //       rows: 2,
+  //       placeholder: '請輸入內容'
+  //     },
+  //     colProps: { span: 12 }
+  //   },
+  //   table: { show: false }
+  // },
+
+  // {
+  //   field: 'prescriptionDivider',
+  //   label: '書寫處方',
+  //   table: { show: false },
+  //   form: {
+  //     show: true,
+  //     component: 'Divider'
+  //   }
+  // },
+  // {
+  //   field: 'pharmacyId',
+  //   label: '',
+  //   form: {
+  //     show: true,
+  //     component: 'Select',
+  //     componentProps: {
+  //       // onChange: handleTypeChange
+  //     },
+  //     colProps: { span: 6 },
+  //     api: async () => {
+  //       return await getInOptionFormat('/recipel/pharmacy', 'pharmacyId', 'pharmacyName')
+  //     }
+  //   },
+  //   table: { show: false }
+  // },
+  // {
+  //   field: 'tempType',
+  //   label: '',
+  //   form: {
+  //     show: true,
+  //     component: 'Select',
+  //     componentProps: {
+  //       options: dict.recipeType
+  //       // onChange: handleTypeChange
+  //     },
+  //     colProps: { span: 6 }
+  //   },
+  //   table: { show: false }
+  // },
+  // {
+  //   field: 'drugSearch',
+  //   label: '醫生：',
+  //   form: {
+  //     component: 'Autocomplete',
+  //     componentProps: {
+  //       style: 'width: 100%',
+  //       triggerOnFocus: false,
+  //       fetchSuggestions: async (queryString: string, cb: Fn) => {
+  //         // formModel.value.
+
+  //         const res = await getApi(
+  //           `recipel/drug?keyWords=${queryString}&pharmacyId=YFgsyf&drugType=ZCY&doctorId=undefined&md5=8568c4b5e086f47d08e8ef6a6a34761c&_=1657384543588`
+  //         )
+  //         const result = res?.data.map((item) => ({
+  //           ...item,
+  //           value: item.doctorName,
+  //           label: item.doctorMobile
+  //         }))
+  //         cb(result)
+  //       },
+  //       onSelect: (item: Recordable) => {},
+  //       slots: {
+  //         default: true
+  //       },
+  //       placeholder: '藥品名稱/拼音'
+  //     },
+  //     colProps: { span: 12 },
+  //     show: true
+  //   }
+  // }
+
+  // // {
+  // //   field: 'templet',
+  // //   label: '短信模板',
+  // //   form: {
+  // //     show: true,
+  // //     component: 'Select',
+  // //     componentProps: {
+  // //       placeholder: '短信模板',
+  // //       options: store.templet,
+  // //       onChange: handleTempletChange
+  // //     },
+  // //     colProps: { span: 12 }
+  // //   },
+  // //   table: { show: false }
+  // // },
+  // // {
+  // //   field: 'content',
+  // //   label: '短信內容',
+  // //   form: {
+  // //     show: true,
+  // //     component: 'Input',
+  // //     componentProps: {
+  // //       placeholder: '短信內容',
+  // //       type: 'textarea',
+  // //       rows: 2
+  // //     },
+  // //     colProps: { span: 24 }
+  // //   },
+  // //   table: { show: false }
+  // // }
+])
+
+const { allSchemas } = useCrudSchemas(crudSchemas)
+
+const dialogVisible = ref(false)
+
+const dialogTitle = ref('')
+const dialogWidth = ref('')
+
+const delLoading = ref(false)
+
+const delData = async (row: MemberInfoTableData | null, multiple: boolean) => {
+  tableObject.currentRow = row
+  const { delList, getSelections } = methods
+  const selections = await getSelections()
+  delLoading.value = true
+  await delList(
+    multiple
+      ? {
+          multiple: multiple,
+          data: selections.reduce((sum, v) => sum + (sum === '' ? '' : ',') + v.id, '') as string
+        }
+      : { multiple: multiple, data: tableObject.currentRow?.id as string },
+    multiple
+  ).finally(() => {
+    delLoading.value = false
+  })
+}
+
+const actionType = ref('')
+
+const AddAction = () => {
+  dialogTitle.value = '門診處方錄入'
+  actionType.value = 'add'
+  dialogWidth.value = '90%'
+  tableObject.currentRow = null
+  dialogVisible.value = true
+}
+
+const writeRef = ref<ComponentRef<typeof Write>>()
+
+const action = (row: TableData, type: string) => {
+  dialogTitle.value = type === 'edit' ? '修改短信模板' : 'exampleDemo.detail'
+  actionType.value = type
+  tableObject.currentRow = row
+  dialogVisible.value = true
+}
+
+const loading = ref(false)
+
+const save = async () => {
+  const write = unref(writeRef)
+  await write?.elFormRef?.validate(async (isValid) => {
+    if (isValid) {
+      loading.value = true
+      const data = (await write?.getFormData()) as any
+      const res = await saveTableApi(
+        actionType.value === 'add'
+          ? data
+          : {
+              id: data.id,
+              label: data.title,
+              type: data.type,
+              content: data.content
+            }
+      )
+        .catch(() => {})
+        .finally(() => {
+          loading.value = false
+        })
+      if (res) {
+        dialogVisible.value = false
+        ElMessage.success(res.msg)
+        tableObject.currentPage = 1
+        getList()
+      }
+    }
+  })
+}
+</script>
+
+<template>
+  <ContentWrap>
+    <Search
+      :schema="allSchemas.searchSchema"
+      :is-col="true"
+      :inline="false"
+      :layout="'bottom'"
+      :buttom-position="'right'"
+      @search="setSearchParams"
+      @reset="setSearchParams"
+    />
+
+    <div class="mb-10px ml-10px mt-[-32px]">
+      <ElButton type="primary" @click="AddAction" :icon="plusIcon">新增</ElButton>
+    </div>
+
+    <Table
+      v-model:pageSize="tableObject.pageSize"
+      v-model:currentPage="tableObject.currentPage"
+      :columns="allSchemas.tableColumns"
+      :data="tableObject.tableList"
+      :loading="tableObject.loading"
+      :pagination="{
+        total: tableObject.total
+      }"
+      @register="register"
+    >
+      <template #action="{ row }">
+        <ElLink type="primary" @click="action(row, 'edit')" class="mr-5px">編輯</ElLink>
+        <ElLink type="danger" @click="delData(row, false)">
+          {{ t('exampleDemo.del') }}
+        </ElLink>
+      </template>
+      <!-- <template #value="{ row }">
+        {{ row.sysDict.value }}
+      </template> -->
+    </Table>
+  </ContentWrap>
+
+  <Dialog v-model="dialogVisible" :title="dialogTitle" :width="dialogWidth">
+    <Write
+      v-if="actionType !== 'detail'"
+      ref="writeRef"
+      :form-schema="allSchemas.formSchema"
+      :current-row="tableObject.currentRow"
+      :params="{
+        type: null
+      }"
+    />
+
+    <template #footer>
+      <ElButton v-if="actionType !== 'detail'" type="primary" :loading="loading" @click="save">
+        {{ t('exampleDemo.save') }}
+      </ElButton>
+      <ElButton @click="dialogVisible = false">{{ t('dialogDemo.close') }}</ElButton>
+    </template>
+  </Dialog>
+</template>
+
+<style lang="less" scoped>
+.el-input__wrapper {
+  width: 100%;
+}
+</style>
