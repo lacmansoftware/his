25e2adb2b35d07771bbad8d18eb55be356626d81
Obrad Lacman <lacmansoftware@gmail.com>
2022-07-21 23:48:43 +0900

    perf: remove junk files in sms/send

--- src/api/workorder/sms/send/index.ts
+++ src/api/workorder/sms/send/index.ts
@@ -17,11 +17,6 @@ export const saveTableApi = async (data: Partial<SMSSendData>): Promise<IRespons
   return res && res.data
 }
 
-export const getTableDetApi = async (id: string): Promise<IResponse<SMSSendData>> => {
-  const res = await request.get({ url: '/example/detail', params: { id } })
-  return res && res.data
-}
-
 export const delTableListApi = async (req: any): Promise<IResponse> => {
   // sms/send/batchdel
   // deldata: 0673867d3e1a4c108c4f6cf0523b2eb9,02669c75df454772b035ae773aeebd0f
@@ -31,20 +26,6 @@ export const delTableListApi = async (req: any): Promise<IResponse> => {
     data: {
       deldata: req
     }
-  })
-  return res && res.data
-}
-
-export const saveMergeApi = async (params: any): Promise<IResponse> => {
-  const res = await request.get({ url: '/member/info/infoMerge', params })
-  return res && res.data
-}
-
-export const updateTableApi = async (data: any): Promise<IResponse> => {
-  const res = await request.post({
-    url: '/member/workorder/update',
-    headersType: 'application/x-www-form-urlencoded',
-    data
   })
   return res && res.data
 }
