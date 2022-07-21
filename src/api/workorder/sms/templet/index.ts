7a42cb31ca267e5760d7e4b79007907eb9dd6d5e
Obrad Lacman <lacmansoftware@gmail.com>
2022-07-21 23:44:09 +0900

    fix: error in delete row

--- src/api/workorder/sms/templet/index.ts
+++ src/api/workorder/sms/templet/index.ts
@@ -22,34 +22,7 @@ export const getTableDetApi = async (id: string): Promise<IResponse<SMSTemplateD
   return res && res.data
 }
 
-export const delTableListApi = async (req: any): Promise<IResponse> => {
-  // sms/send/batchdel
-  // deldata: 0673867d3e1a4c108c4f6cf0523b2eb9,02669c75df454772b035ae773aeebd0f
-  if (req?.multiple) {
-    const res = await request.post({
-      url: '/sms/templet/batchdel',
-      headersType: 'application/x-www-form-urlencoded',
-      data: {
-        deldata: req.data
-      }
-    })
-    return res && res.data
-  } else {
-    const res = await request.delete({ url: `/sms/templet/${req.data}` })
-    return res && res.data
-  }
-}
-
-export const saveMergeApi = async (params: any): Promise<IResponse> => {
-  const res = await request.get({ url: '/member/info/infoMerge', params })
-  return res && res.data
-}
-
-export const updateTableApi = async (data: any): Promise<IResponse> => {
-  const res = await request.post({
-    url: '/member/workorder/update',
-    headersType: 'application/x-www-form-urlencoded',
-    data
-  })
+export const delTableListApi = async (req: string): Promise<IResponse> => {
+  const res = await request.delete({ url: `/sms/templet/${req}` })
   return res && res.data
 }
